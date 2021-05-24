0bca91b14828e91120938e8097f842c5fc10964a
raisework <ilha.dev.br@gmail.com>
2021-05-24 13:05:08 -0300

    Add ExpenseDate styles

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Expense Tracker</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
