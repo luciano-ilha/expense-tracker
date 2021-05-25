fa4ec1ef13ebc2ab3651f69ae749c86079656045
raisework <ilha.dev.br@gmail.com>
2021-05-25 17:04:34 -0300

    Set a handler to submit form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,3 @@
-import { use } from "matter";
 import { useState } from "react";
 import "./ExpenseForm.css";
 
@@ -47,8 +46,20 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
