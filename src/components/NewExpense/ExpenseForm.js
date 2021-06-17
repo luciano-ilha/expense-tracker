29b00b4c76804fd64880c0a8349b073946f603f3
Luciano Ilha <ilha.dev.br@gmail.com>
2021-06-17 15:48:28 -0300

    Add onClick display change to new expense user inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     //   return { ...prevState, enteredTitle: event.target.value };
     // });
   };
-  
+
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
     // setUserInput({
@@ -94,6 +94,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
